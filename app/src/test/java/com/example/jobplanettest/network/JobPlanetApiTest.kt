82f03eb731eae0206724345172a9bbf52869affd
최우성 <vov1252@naver.com>
2022-10-04 00:54:44 +0900

    [add] 테마, string , ansrn, 폰트, shape 추가

--- app/src/test/java/com/example/jobplanettest/network/JobPlanetApiTest.kt
+++ app/src/test/java/com/example/jobplanettest/network/JobPlanetApiTest.kt
@@ -1,6 +1,7 @@
 package com.example.jobplanettest.network
 
 import MainCoroutinesRule
+import com.example.jobplanettest.model.enums.CellTypeEnum
 import com.google.common.truth.Truth
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
@@ -35,4 +36,39 @@ class LoginApiTest : ApiAbstract<JobPlanetAPI>() {
     }
 
 
+    @Throws(IOException::class)
+    @Test
+    fun `기업데이터 가져왔을떄_CellTypeEnum이 제대로 변환되는가?`() {
+        runBlocking {
+            enqueueResponse("/enterprise_items.json")
+            val response = service.fetchEnterpriseItems()
+            mockWebServer.takeRequest()
+            Truth.assertThat(response.cell_items[0].cell_type).isEqualTo(CellTypeEnum.CELL_TYPE_COMPANY)
+        }
+    }
+
+    @Throws(IOException::class)
+    @Test
+    fun `기업데이터 가져왔을떄_3번쨰 item이_CELL_TYPE_HORIZONTAL_THEME 인가?`() {
+        runBlocking {
+            enqueueResponse("/enterprise_items.json")
+            val response = service.fetchEnterpriseItems()
+            mockWebServer.takeRequest()
+            Truth.assertThat(response.cell_items[2].cell_type)
+                .isEqualTo(CellTypeEnum.CELL_TYPE_HORIZONTAL_THEME)
+        }
+    }
+
+    @Throws(IOException::class)
+    @Test
+    fun `채용데이터 가져왔을떄_appeal이 분리되는가?`() {
+        runBlocking {
+            enqueueResponse("/recruit_items.json")
+            val response = service.fetchRecruitItems()
+            mockWebServer.takeRequest()
+            val splitResult = response.recruitItems.get(0).appeal.split(", ")
+            Truth.assertThat(splitResult[0]).isEqualTo("복지포인트")
+            Truth.assertThat(splitResult[1]).isEqualTo("스낵바")
+        }
+    }
 }
